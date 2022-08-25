729cfa4dfbf42a6ed5463aafe24c5d3c60b89393
Thien Nguyen <thiennc1107@gmail.com>
2022-08-25 23:56:34 +0700

    go + react

---
+++ server/go.mod
@@ -0,0 +1,18 @@
+module github.com/thiennc1107/go_todo
+
+go 1.18
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/gofiber/fiber/v2 v2.36.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/mattn/go-sqlite3 v1.14.15 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.39.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220823224334-20c2bfdbfe24 // indirect
+	gorm.io/driver/sqlite v1.3.6 // indirect
+	gorm.io/gorm v1.23.8 // indirect
+)
---
+++ server/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Todo struct {
+	ID    int    `json:"id"`
+	Title string `json:"title,omitempty"`
+	Done  bool   `json:"done"`
+	Body  string `json:"body,omitempty"`
+}
+
+func main() {
+	log.Println("Connecting database")
+	db, err := gorm.Open(sqlite.Open("../todo.db"), &gorm.Config{})
+
+	getAll := func() ([]Todo, error) {
+		var todos []Todo
+		result := db.Find(&todos)
+		if result.Error != nil {
+			return nil, result.Error
+		}
+		return todos, nil
+	}
+
+	db.AutoMigrate(&Todo{})
+
+	if err != nil {
+		panic("Failed to connect db:" + err.Error())
+	}
+
+	fmt.Println("Initializing server")
+
+	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:3000",
+		AllowMethods: strings.Join([]string{
+			fiber.MethodGet,
+			fiber.MethodPost,
+			fiber.MethodPatch,
+		}, ","),
+	}))
+
+	app.Get("/healthcheck", func(c *fiber.Ctx) error {
+		return c.SendString("Ok")
+	})
+
+	app.Patch("/api/todos/:id/done", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+		if err != nil {
+			return c.Status(401).SendString("Invalid id")
+		}
+		var todo Todo
+		result := db.Model(&todo).Where("ID = ?", id).Update("Done", true)
+		if result.Error != nil {
+			return result.Error
+		}
+
+		todos, err := getAll()
+		if err != nil {
+			return err
+		}
+
+		return c.JSON(todos)
+	})
+
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
+		todos, err := getAll()
+		if err != nil {
+			return err
+		}
+		return c.JSON(todos)
+	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+		result := db.Create(&todo)
+
+		if result.Error != nil {
+			return result.Error
+		}
+		todos, err := getAll()
+		if err != nil {
+			return err
+		}
+		return c.JSON(todos)
+	})
+
+	log.Println("Server is running on port 4000")
+	log.Fatal(app.Listen(":4000"))
+}
